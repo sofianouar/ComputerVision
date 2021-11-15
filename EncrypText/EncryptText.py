843cbefac50f89d829cd1972446cd4b35e3a2a4f
sopiie <soffianouar@gmail.com>
2021-11-15 17:13:00 +0100

    nnn

--- EncrypText/EncryptText.py
+++ EncrypText/EncryptText.py
@@ -1,1 +1,17 @@
-print('hello test')
+print('Hello, enter your text but size should not be sup 200 caracters')
+
+def ScanImage () :
+	pass
+
+def ScanText () :
+	pass
+
+def EncodeMessage() :
+	pass
+
+def DecodeMessage():
+	pass
+
+
+if __name__ == '__main__':
+	print('Hello')
